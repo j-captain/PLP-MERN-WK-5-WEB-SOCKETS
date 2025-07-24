a426865e139e12e85b9c57e23454b1dd9a282368
j-captain <jkarranja@gmail.com>
2025-07-24 15:27:16 +0300

    ccc
    ccc

--- socketio-chat/client/src/pages/AuthPage.jsx
+++ socketio-chat/client/src/pages/AuthPage.jsx
@@ -1,13 +1,18 @@
 import { useState } from 'react';
 import AuthForm from '../components/AuthForm';
 
+
+
+const API_URL = "https://plp-mern-wk-5-web-sockets-backened-0nno.onrender.com/api";
+
+
 export default function AuthPage({ onAuthSuccess }) {
   const [error, setError] = useState('');
 
   const handleAuth = async (username, password, isLogin) => {
     try {
       const endpoint = isLogin ? '/api/login' : '/api/register';
-      const response = await fetch(`http://localhost:5000${endpoint}`, {
+      const response = await fetch(`https://plp-mern-wk-5-web-sockets-backened-0nno.onrender.com/api${endpoint}`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username, password }),
