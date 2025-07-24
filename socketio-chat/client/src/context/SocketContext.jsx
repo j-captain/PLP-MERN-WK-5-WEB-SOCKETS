74c8b370a9f8fa6524fa57ed13218db16da7afd7
j-captain <jkarranja@gmail.com>
2025-07-24 16:56:08 +0300

    xx
    xx

--- socketio-chat/client/src/context/SocketContext.jsx
+++ socketio-chat/client/src/context/SocketContext.jsx
@@ -10,7 +10,7 @@ export const SocketProvider = ({ children, username }) => {
   useEffect(() => {
     if (!username) return;
 
-    const newSocket = io('http://localhost:5000', {
+    const newSocket = io('https://plp-mern-wk-5-web-sockets-backened-0nno.onrender.com', {
       auth: { username },
       withCredentials: true
     });
